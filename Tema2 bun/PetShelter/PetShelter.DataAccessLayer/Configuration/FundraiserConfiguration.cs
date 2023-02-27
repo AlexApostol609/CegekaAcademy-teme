37754db4f0a113b376fbd2d0c0cf0ab76a7e1ce1
AlexApostol609 <74296118+AlexApostol609@users.noreply.github.com>
2023-02-27 23:30:36 +0200

    Add files via upload

---
+++ Tema2 bun/PetShelter/PetShelter.DataAccessLayer/Configuration/FundraiserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using PetShelter.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace PetShelter.DataAccessLayer.Configuration
+{
+    public class FundraiserConfiguration : IEntityTypeConfiguration<Fundraiser>
+    {
+        public void Configure(EntityTypeBuilder<Fundraiser> builder)
+        {
+            //Primary key
+            builder.HasKey(p => p.Id);
+
+            //Columns mapping and constraints
+            builder.Property(p => p.Title).IsRequired().HasMaxLength(255);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(255);
+            builder.Property(p => p.Target).IsRequired();
+
+            //Relationships
+            builder.HasMany(f => f.Donors).WithOne(p => p.FundraiserId).HasForeignKey(f => f.Id)
+            .IsRequired();
+
+        }
+    }
+}
